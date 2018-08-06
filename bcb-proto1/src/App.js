021b80df46d60c6884fad450b3eb1c5fc92ab315
SanjanaMalandkar <40349894+SanjanaMalandkar@users.noreply.github.com>
2018-08-06 20:24:17 +0530

    bcb-proto-1 update-1

---
+++ bcb-proto1/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import MenuBar from './Components/MenuBar';
+import {Layout,Header,Textfield,Drawer,Navigation,Content,HeaderRow} from 'react-mdl';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MenuBar />
+
+      </div>
+    );
+  }
+}
+
+export default App;
